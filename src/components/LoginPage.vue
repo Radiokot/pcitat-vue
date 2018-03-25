7fd6ef09cd5133567cd74522a8b87a8e381d5c12
Oleg Koretsky <test.radiokot@gmail.com>
2018-03-25 14:28:03 +0300

    Add log in with Twitter button

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12">
-                    <h1 class="text-center"><img src="static/icon/icon.svg" width="120"><br>Просто цитатник</h1>
+                    <h2 class="text-center"><img src="static/icon/icon.svg" width="120"><br>Просто цитатник</h2>
                     <br>
                 </div>
                 <div class="col-md-4 col-md-offset-4">
@@ -21,7 +21,8 @@
                         <button type="submit" id="loginButton" :class="['btn', 'btn-info', 'col-xs-12', !loginButtonEnabled ? 'disabled' : '']">Войти</button>
                     </form>
                     <br style="clear:both;">
-                    <br>
+                    <br><br>
+                    <a id="loginWithTwitter" class="btn btn-twitter col-xs-12" :href="twitterAuthUrl"><img src="static/icon/twitter-icon.svg">&nbsp;Войти через Twitter</a>
                     <button class="btn btn-default col-xs-12" @click="signUp">Зарегистрироваться</button>
                     <br style="clear: both;">
                     <br>
@@ -62,6 +63,11 @@
                 signupErrorMessage: ''
             }
         },
+        computed: {
+            twitterAuthUrl() {
+                return process.env.API_URL + "twitterOauth?action=login"
+            }
+        },
         created() {
             this.$root.$data.activeTab = 3
 
@@ -86,16 +92,39 @@
             },
             logIn() {
                 Users(this.$root).logIn(this.loginData)
-                    .then(user => {
-                        this.$root.bus.$emit('new-user', user)
-                        this.loginButtonEnabled = true
-                    })
-                    .catch(error => {
-                        this.loginButtonEnabled = true
-                        this.loginError = true
-                    })
+                .then(user => {
+                    this.$root.bus.$emit('new-user', user)
+                    this.loginButtonEnabled = true
+                })
+                .catch(error => {
+                    this.loginButtonEnabled = true
+                    this.loginError = true
+                })
             }
         }
     }
 
 </script>
+
+<style>
+    #loginWithTwitter {
+        margin-bottom: 20px;
+    }
+    #loginWithTwitter img {
+        width: 1em;
+        vertical-align: baseline;
+    }
+    .btn-twitter {  
+       color: #ffffff;
+       background-color: #1da1f2;
+       border-color: #1da1f2;
+   }
+   .btn-twitter:hover {
+        color: #ffffff;
+        background-color: #006dbf;
+        border-color: #006dbf;
+    }
+    body {
+        margin-bottom: 0;
+    }
+</style>
